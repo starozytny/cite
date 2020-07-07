66fb07036dc0536a64a6b7329bb5dccdffc0b244
Chanbora <chanbora@logilink.fr>
2020-07-07 09:10:09 +0200

    split code

--- assets/themes/default/front/js/pages/components/booking/Booking.jsx
+++ assets/themes/default/front/js/pages/components/booking/Booking.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Input} from '../../../components/composants/Fields';
+import {StepProspects} from './Prospect';
 
 export class Booking extends Component {
     constructor(props){
@@ -22,7 +22,7 @@ export class Booking extends Component {
 
     render () {
 
-        const {classStart, classStep1, nbPersonne} = this.state;
+        const {classStart, classStep1} = this.state;
 
         return <>
             <section className={"section-infos " + classStart}>
@@ -31,153 +31,11 @@ export class Booking extends Component {
             </section>
             <section className="section-steps">
                 <div className="steps">
-                    <Step1 classStep={classStep1}/>
+                    <StepProspects classStep={classStep1}/>
                 </div>
             </section>
         </>
     }
-}
-
-/**
-    Step  : Récupérer les informations de chaque personnes à inscrire
- */
-export class Step1 extends Component {
-    constructor(props){
-        super(props)
-        
-        this.state = {
-            added: 0,
-            deleted: 0,
-            classAdd: ''
-        }
-
-        this.handleClickDelete = this.handleClickDelete.bind(this); 
-        this.handleClickAdd = this.handleClickAdd.bind(this); 
-
-        this.handleClickNext = this.handleClickNext.bind(this);
-    }
-
-    /**
-        Gestion d'ajout et suppression d'inscrits
-     */
-    handleClickDelete (e) {
-        this.setState({deleted: parseInt(this.state.deleted) + 1, classAdd: ''})
-    }
-    handleClickAdd (e) {
-        let value = parseInt(this.state.added) + 1;
-        let valueDeleted = parseInt(this.state.deleted);
-        let remaining = value - valueDeleted;
-        if(remaining < 10){
-            this.setState({added: value});
-        }else if (remaining === 10){
-            this.setState({added: value, classAdd: 'full'});
-        }else{
-             this.setState({classAdd: 'full'});
-        }
-    }
-
-    /**
-        Gestion étape suivante
-     */
-    handleClickNext (e) {
-    }
-
-    render () {
-        const {classStep} = this.props;
-        const {added, classAdd} = this.state;
-
-        let arr = [];
-        for (let i=0 ; i<added ; i++) {
-            arr.push(
-                <Prospect key={i} id={i} onDeleteCard={this.handleClickDelete}/>
-            )
-        }
-        
-        let body = <>
-            <div className="step-prospects">
-                {arr}
-            </div>
-            <div className={"step-prospects-add " + classAdd}>
-                <button onClick={this.handleClickAdd}>Ajouter</button>
-            </div>
-        </>
-
-        return <Step id="1" classStep={classStep} title="Informations" onClickNext={this.handleClickNext} body={body}>
-            Les informations recueillies à partir de ce formulaire sont transmises au service de la Cité de la musique dans le but 
-            de pré-remplir les inscriptions. Plus d'informations sur le traitement de vos données dans notre 
-            politique de confidentialité.
-        </Step>
-    }
-}
-
-class Prospect extends Component {
-    constructor(props){
-        super(props)
-
-        this.state = {
-            renderCompo: true,
-            valide: false,
-            firstname: {value: '', error: ''},
-            lastname: {value: '', error: ''},
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleDelete (e) {
-        this.setState({renderCompo: false})
-        this.props.onDeleteCard();
-    }
-
-    handleChange (e) {
-        let name = e.target.name;
-        this.setState({ [name.substr(0,name.indexOf("-"))]: {value: e.target.value} });
-    }
-
-    handleClick (e) {
-
-    }
-
-    render () {
-        const {firstname, lastname, renderCompo} = this.state;
-        const {id} = this.props;
-
-        return <>
-            {renderCompo ? <ProspectCard id={id} firstname={firstname} lastname={lastname} onChange={this.handleChange} onDelete={this.handleDelete} onClick={this.handleClick} /> : null}
-        </>
-    }
-} 
-
-function ProspectCard({id, firstname, lastname, 
-                        onChange, onDelete, onClick}) 
-    {
-    return <div className="step-card step-prospect">
-        <Input type="text" identifiant={"firstname-" + id} value={firstname.value} onChange={onChange} error={firstname.error}>Prénom</Input>
-        <Input type="text" identifiant={"lastname-" + id} value={lastname.value} onChange={onChange} error={lastname.error}>Nom</Input>
-
-        <button onClick={onDelete}>Supprimer</button>
-        <button onClick={onClick}>Valider</button>
-    </div>
-}
-
-function Step({id, classStep, title, body, onClickNext, onClickPrev, children}) {
-    return (
-        <div className={"step step-" + id + " " + classStep}>
-            <div className="step-title">
-                <h2>{title}</h2>
-                {children ? <p>{children}</p> : null}
-            </div>
-            <div className="step-content">
-                {body}
-            </div>
-            <div className="step-actions">
-                <button className="btn btn-back" onClick={onClickPrev}>Retour</button>
-                <button className="btn btn-primary" onClick={onClickNext}>Suivant</button>
-            </div>
-        </div>
-    )
 }
 
 function Starter({onClick}) {
---
+++ assets/themes/default/front/js/pages/components/booking/Step.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export function Step({id, classStep, title, body, onClickNext, onClickPrev, children}) {
+    return (
+        <div className={"step step-" + id + " " + classStep}>
+            <div className="step-title">
+                <h2>{title}</h2>
+                {children ? <p>{children}</p> : null}
+            </div>
+            <div className="step-content">
+                {body}
+            </div>
+            <div className="step-actions">
+                <button className="btn btn-back" onClick={onClickPrev}>Retour</button>
+                <button className="btn btn-primary" onClick={onClickNext}>Suivant</button>
+            </div>
+        </div>
+    )
+}
