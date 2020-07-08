d107a14d60d3fc3b8e3b888b9b56e61e67af5c2b
Chanbora <chanbora@logilink.fr>
2020-07-08 12:14:59 +0200

    init style admin

--- assets/themes/default/back/js/default.jsx
+++ assets/themes/default/back/js/default.jsx
@@ -1,1 +1,5 @@
 import '../css/default.scss';
+const routes = require('../../../../../public/js/fos_js_routes.json');
+import Routing from '../../../../../public/bundles/fosjsrouting/js/router.min.js';
+
+Routing.setRoutingData(routes);
