074f79daa7252f2251cfdd558956f8e024d2395b
Chanbora <chanbora@logilink.fr>
2020-07-17 14:52:53 +0200

    style main homepage

--- assets/themes/default/front/js/pages/homepage.jsx
+++ assets/themes/default/front/js/pages/homepage.jsx
@@ -1,14 +1,3 @@
-import '../../css/pages/security.scss';
+import '../../css/pages/homepage.scss';
 import React, {Components} from 'react';
 import ReactDOM from 'react-dom';
-import Compteur from '../components/composants/Compteur';
-
-experience();
-
-function experience(){
-    let y = new Date();
-    ReactDOM.render(
-        <Compteur max={y.getFullYear() - parseInt(document.querySelector('#r-compteur').dataset.count)}  timer="25"/>,
-        document.getElementById('r-compteur')
-    );
-}
