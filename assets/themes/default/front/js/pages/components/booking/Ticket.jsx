ddb6570e1c867ace0864ee5e6ff5677069ec44ac
Chanbora <chanbora@logilink.fr>
2020-07-22 14:48:25 +0200

    pdf ok

--- assets/themes/default/front/js/pages/components/booking/Ticket.jsx
+++ assets/themes/default/front/js/pages/components/booking/Ticket.jsx
@@ -18,7 +18,7 @@ export class StepTicket extends Component {
                         <img src={ "data:image/png;base64," + barcode } />
                     </div>
                     <div>Pour le {day} à {horaire}.</div>
-                    <div className="ticket-email">Votre ticket a été envoyé à <b>{finalMessage}</b> en <b>pièce jointe</b>. <br/> Veuillez vérifiez vos spams/courriers indésirables.</div>
+                    <div className="ticket-email"><span>Votre ticket a été envoyé à <b>{finalMessage}</b> en <b>pièce jointe</b>.</span> <br/> Veuillez vérifiez vos spams/courriers indésirables.</div>
                     <div className="ticket-download">
                         <a href={print} target="_blank">Imprimer mon billet</a>
                     </div>
