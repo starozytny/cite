6f34e4f3af329b8a5f1c4939d6aa0d567da5b168
Chanbora <chanbora@logilink.fr>
2020-07-31 10:16:34 +0200

    add cp database

--- bin/importcp.sh
+++ bin/importcp.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-cd code_postaux || return 2
-for file in *
-  do
-    firstcol=$(csvcut -d ";" -c 1 $file | head -n 1)
-    csvsql -i mysql 
-    filename="data_${file%.*}"
-    csvsql --db mysql+mysqlconnector://root:@localhost:3306/cite --overwrite --tables $filename --insert $file
-done
+file=code_postaux/CODE_POSTAUX.csv
+firstcol=$(csvcut -d ";" -c 1 $file | head -n 1)
+csvsql -i mysql $file
+filename="data_code_postaux"
+csvsql --db mysql+mysqlconnector://root:@localhost:3306/cite --overwrite --tables $filename --insert $file
