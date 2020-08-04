1d6c3a2df4c5155d7ea4ac672f3197a21b009747
Chanbora <chanbora@logilink.fr>
2020-08-04 14:58:49 +0200

    connnection sql sh

--- bin/import.sh
+++ bin/import.sh
@@ -16,6 +16,6 @@ for file in *
     firstcol=$(csvcut -d ";" -c 1 $file | head -n 1)
     csvsql -i mysql $file
     filename="windev_${file%.*}"
-    csvsql --db mysql+mysqlconnector://root:@localhost:3306/cite --overwrite --tables $filename --insert $file --query "ALTER TABLE ${filename} ADD PRIMARY KEY (${firstcol}); ALTER TABLE ${filename} CHANGE ${firstcol} id INT AUTO_INCREMENT NOT NULL;  ALTER TABLE ${filename} DROP BASE;"
+    csvsql --db mysql+mysqlconnector://citemuse_webservice:ErhY6QUOGCKR@localhost:3306/citemuse_webservice --overwrite --tables $filename --insert $file --query "ALTER TABLE ${filename} ADD PRIMARY KEY (${firstcol}); ALTER TABLE ${filename} CHANGE ${firstcol} id INT AUTO_INCREMENT NOT NULL;  ALTER TABLE ${filename} DROP BASE;"
     mv $file ../traiter/$file
 done
--- bin/importcp.sh
+++ bin/importcp.sh
@@ -4,4 +4,4 @@ file=code_postaux/CODE_POSTAUX.csv
 firstcol=$(csvcut -d ";" -c 1 $file | head -n 1)
 csvsql -i mysql $file
 filename="data_code_postaux"
-    csvsql --db mysql+mysqlconnector://root:@localhost:3306/cite --overwrite --tables $filename --insert $file --query "ALTER TABLE ${filename} ADD PRIMARY KEY (${firstcol}); ALTER TABLE ${filename} CHANGE ${firstcol} id INT AUTO_INCREMENT NOT NULL;"
+    csvsql --db mysql+mysqlconnector://citemuse_webservice:ErhY6QUOGCKR@localhost:3306/citemuse_webservice --overwrite --tables $filename --insert $file --query "ALTER TABLE ${filename} ADD PRIMARY KEY (${firstcol}); ALTER TABLE ${filename} CHANGE ${firstcol} id INT AUTO_INCREMENT NOT NULL;"
