36eeddff642cf646a19ab753ea73ce5dce44c798
Chanbora <chanbora@logilink.fr>
2020-09-01 10:44:41 +0200

    new email sender

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -20,7 +20,7 @@ class Mailer
         $this->publicDirectory = $publicDirectory;
     }
 
-    public function sendMail($title, $text, $html, $params, $email,  $file = null, $responsable=null, $from = 'inscriptions@citemusique-marseille.com')
+    public function sendMail($title, $text, $html, $params, $email,  $file = null, $responsable=null, $from = 'no-reply@inscriptions-musique-marseille.com')
     {
         $email = (new TemplatedEmail())
             ->from($from)
